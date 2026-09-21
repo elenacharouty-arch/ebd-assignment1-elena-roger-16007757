b2147cb6d2a7b54c3c0e47285bafd689604950db
Elena Charouty <elena.charouty@student.giu-uni.de>
2026-09-21 21:06:19 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,9 @@
+console.log("Elena the Rogue");
+
+console.log("16007757");
+
+let studentId = 16007757;
+let power = studentId * 9;
+console.log(power);
+
+console.log("\"By the Delta marshes, the Sand Kraken yields!\"");
